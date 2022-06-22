d15a9d730730d4d81134bfbe12498ada68620580
Fedor Alfimov <alfimov.fedor@gmail.com>
2022-06-22 10:00:38 +0800

    worked on suspects and stuff

---
+++ react-task-tracker/src/components/Button.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Button = ({color,text}) => {
+  return (
+    <div>
+        <button style={{backgroundColor:color}}className="btn">{text}</button>
+    </div>
+  )
+}
+export default Button
